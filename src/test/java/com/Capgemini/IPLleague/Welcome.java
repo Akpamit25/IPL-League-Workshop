e1d4a819cd690b64d97bc730321caef7e8e0d043
Amit Kumar <amitnitsri25@gmail.com>
2020-10-21 09:47:07 +0530

    Welcome To IPL League Analyzer

---
+++ src/test/java/com/Capgemini/IPLleague/Welcome.java
@@ -0,0 +1,21 @@
+package com.Capgemini.IPLleague;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This Is IPL Analyzer Workshop" );
+	}
+
+}
+
+/* Problem Details : 
+ 
+1. Workshop Problem - IPL Analyser Workshop. You should be able to demonstrate the following: 
+2. Libraries – OpenCSV, JSON
+3. Generics, Sort and Search
+4. Ability to write Logical Code 
+5. Ability to use Java Stream 
+6. Use of Design Principles
+
+*/
